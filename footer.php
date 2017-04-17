e03759e6ccce9bf2d99eb6e5b3c19a1812f665b5
Reggie <revital.peled@gmail.com>
2017-04-17 18:42:14 -0400

    Add files via upload

--- footer.php
+++ footer.php
@@ -9,11 +9,11 @@
 		<article>
             <a class="social" href="https://www.facebook.com/revital.peled.10" target="_blank" title="Facebook">
                 <img src="<?php echo get_theme_file_uri('images/facebook.png');?>" alt="Facebook" title="Facebook" /></a>
-            <a class="social" href="https://www.pinterest.com/revitalpeled/" target="_blank" title="Twitter">
+            <a class="social" href="https://twitter.com/ReggiePeled" target="_blank" title="Twitter">
                 <img src="<?php echo get_theme_file_uri('images/twitter.png');?>" alt="Twitter" title="Twitter" /></a>
-            <a class="social" href="https://www.pinterest.com/revitalpeled/" target="_blank" title="Instagram">
+            <a class="social" href="https://www.instagram.com/peledreggie/?hl=en" target="_blank" title="Instagram">
                 <img src="<?php echo get_theme_file_uri('images/instagram.png');?>" alt="Instagram" title="Instagram" /></a>	
-            <a class="social" href="https://www.pinterest.com/revitalpeled/" target="_blank" title="Blog">
+            <a class="social" href="http://www.designedbyreggie.com/businesssite/blog/" target="_blank" title="Blog">
                 <img src="<?php echo get_theme_file_uri('images/blogsite.png');?>" alt="Blog" title="Blog" /></a>
         </article>
 		<p>800 - 510 - 1670</p>
