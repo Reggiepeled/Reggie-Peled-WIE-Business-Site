36ed71100ecfdde80603dba1c63ed67a7079e448
Reggie <revital.peled@gmail.com>
2017-04-17 10:58:38 -0400

    Add files via upload

--- search.php
+++ search.php
@@ -1,23 +1,25 @@
 <?php get_header(); ?>
 
-<div class="row">
-	<div class="nine columns">
-		<?php if( have_posts() ) :?>
-			<h1><?php printf(
-				__('Search Results for: %s'), 
-				'<span>' . get_search_query() . '</span>' );
-			?></h1>
-			<?php while (have_posts()) : the_post(); ?>
-				<h2><?php the_title(); ?></h2>
-				<?php the_content();
-			endwhile;
-		 else : ?>
-			<h1>Nothing Found</h1>
-			<p>Sorry, but nothing matched your search criteria. Please try again with different search terms.</p>
-		<?php endif; ?>
-	</div>
-	<div class="three columns">
-		<?php get_sidebar(); ?>
+<div class="container search-wrapper">
+	<div class="row">
+		<div class="nine columns">
+			<?php if( have_posts() ) :?>
+				<h2><?php printf(
+					__('Search Results for: %s'), 
+					'<span>' . get_search_query() . '</span>' );
+				?></h2>
+				<?php while (have_posts()) : the_post(); ?>
+					<h3><?php the_title(); ?></h3>
+					<?php the_content();
+				endwhile;
+			 else : ?>
+				<h2>Nothing Found</h2>
+				<p>Sorry, but nothing matched your search criteria. Please try again with different search terms.</p>
+			<?php endif; ?>
+		</div>
+		<div class="three columns">
+			<?php get_sidebar(); ?>
+		</div>
 	</div>
 </div>
 
